906e911b8f6a4726b579c7f8df4ef4ad52537bf6
Sergey <xxsmotur@gmail.com>
2020-02-20 16:29:21 +0500

    Draft for saving data

--- internal/serializer/json/serializer.go
+++ internal/serializer/json/serializer.go
@@ -2,16 +2,16 @@ package json
 
 import json "github.com/pquerna/ffjson/ffjson"
 
-// JSONSerializer defines...
-type JSONSerializer struct {
+// Serializer defines...
+type Serializer struct {
 }
 
 // Marshal provides serialization of object
-func(j *JSONSerializer) Marshal(d interface{}) ([]byte, error) {
+func (j *Serializer) Marshal(d interface{}) ([]byte, error) {
 	return json.Marshal(d)
 }
 
 // Unmarshal provides deserialization of object
-func(j *JSONSerializer) Unmarshal(data []byte, res interface{}) error {
+func (j *Serializer) Unmarshal(data []byte, res interface{}) error {
 	return json.Unmarshal(data, res)
 }
--- internal/storage/save.go
+++ internal/storage/save.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/saromanov/re-orm/internal/reflect"
+	"github.com/saromanov/re-orm/internal/serializer/json"
 )
 
 // Save provides saving of the object
@@ -16,6 +17,22 @@ func Save(d interface{}) (string, error) {
 	if err != nil {
 		return "", fmt.Errorf("unable to get fields from provided data: %v", err)
 	}
+
 	fmt.Println("FIELDS: ", fields)
 	return "", nil
+}
+
+// save provides saving of the model
+func save(ID interface{}, d interface{}) error {
+	ser := json.Serializer{}
+	key := fmt.Sprintf("id_%v", ID)
+	result, err := ser.Marshal(d)
+	if err != nil {
+		return fmt.Errorf("unable to marshal data with id %v: %v", d.ID, err)
+	}
+	err = r.client.Do("SET", key, string(result)).Err()
+	if err != nil {
+		return fmt.Errorf("unable to set data: %v", err)
+	}
+	return nil
 }
