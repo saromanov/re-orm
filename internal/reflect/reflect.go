dca709dd10555db6006489d8efeb2b3bc6b9343f
Sergey <xxsmotur@gmail.com>
2020-02-20 15:21:47 +0500

    Extend method for getting fields from the input

--- internal/reflect/reflect.go
+++ internal/reflect/reflect.go
@@ -3,6 +3,8 @@ package reflect
 import (
 	"fmt"
 	"reflect"
+
+	"github.com/saromanov/re-orm/internal/models"
 )
 
 // IsAvailableForSave provides check if input data is available for save
@@ -11,23 +13,28 @@ func IsAvailableForSave(d interface{}) bool {
 }
 
 // GetFields provides getting fields from the struct
-func GetFields(d interface{}) {
-	if ok := isStruct(d); !ok {
-		return
+func GetFields(d interface{}) (*models.Data, error) {
+	if ok := IsAvailableForSave(d); !ok {
+		return nil, fmt.Errorf("unable to save provided data")
 	}
 
+	return getFields(d)
 }
 
 // getFields returns name of fields from the structure
-func getFields(d interface{}) {
+func getFields(d interface{}) (*models.Data, error) {
 	s := reflect.ValueOf(d).Elem()
 	typeOfT := s.Type()
-
+	resp := &models.Data{}
 	for i := 0; i < s.NumField(); i++ {
 		f := s.Field(i)
 		fmt.Printf("%d: %s %s = %v\n", i,
 			typeOfT.Field(i).Name, f.Type(), f.Interface())
 	}
+	if resp.ID == nil {
+		return nil, fmt.Errorf("id is not defined")
+	}
+	return resp, nil
 }
 
 // isStruct provides checking if input data is a struct
--- internal/storage/save.go
+++ internal/storage/save.go
@@ -7,9 +7,15 @@ import (
 )
 
 // Save provides saving of the object
-func Save(data interface{}) (string, error) {
-	if ok := reflect.IsAvailableForSave(data); !ok {
+func Save(d interface{}) (string, error) {
+	if ok := reflect.IsAvailableForSave(d); !ok {
 		return "", fmt.Errorf("save: input valus is a not struct")
 	}
+
+	fields, err := reflect.GetFields(d)
+	if err != nil {
+		return "", fmt.Errorf("unable to get fields from provided data: %v", err)
+	}
+	fmt.Println("FIELDS: ", fields)
 	return "", nil
 }
