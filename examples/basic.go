57e3e8a5ad4a5747f6cd97306559968d49ae76d3
Sergey <xxsmotur@gmail.com>
2020-03-04 21:16:52 +0500

    update example

--- examples/basic.go
+++ examples/basic.go
@@ -7,30 +7,55 @@ import (
 )
 
 type Car struct {
-	ID    int64
-	Name  string
-	Color string
+	ID         int64
+	Name       string `reorm:"index"`
+	Color      string `reorm:"index"`
+	Attributes Attributes
+}
+
+type Attributes struct {
+	Windows int64
+	Smart   bool
 }
 
 func main() {
 	c := &Car{
 		ID:    1,
-		Name:  "BMW",
-		Color: "Black",
+		Name:  "BMW1",
+		Color: "Black1",
+		Attributes: Attributes{
+			Windows: 4,
+			Smart:   true,
+		},
+	}
+
+	c2 := &Car{
+		ID:    2,
+		Name:  "Mercedes1",
+		Color: "Black1",
+		Attributes: Attributes{
+			Windows: 3,
+			Smart:   true,
+		},
 	}
 	r := reorm.New(&reorm.Config{})
 	fmt.Println(r.Save(c))
-
+	fmt.Println(r.Save(c2))
 	var resp Car
-	if err := r.GetByID(1, &resp); err != nil {
+	if err := r.GetByID("Car", 1, &resp); err != nil {
 		panic(err)
 	}
 	fmt.Println("RESP: ", resp)
-	if err := r.DeleteByID(1); err != nil {
+	/*if err := r.DeleteByID(1); err != nil {
 		panic(err)
 	}
 	if err := r.GetByID(1, &resp); err != nil {
 		panic(err)
 	}
-	fmt.Println("RESP: ", resp)
+	fmt.Println("RESP: ", resp)*/
+	var respAll []Car
+	r.Find(&Car{
+		Color: "Black",
+	}, &respAll)
+	fmt.Println("RERR: ", respAll)
 }
