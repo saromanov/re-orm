879e3d608752ff0a51ee926c6853bfce49d73c0b
Sergey <xxsmotur@gmail.com>
2020-03-12 01:13:11 +0500

    Add inner method get and rename First to Last

--- internal/storage/get.go
+++ internal/storage/get.go
@@ -13,52 +13,38 @@ import (
 
 // Get provides getting of the saved data by request
 func Get(client *redis.Client, req, data interface{}) error {
-
-	fields, err := reflect.GetFullFields(req)
-	if err != nil {
-		return fmt.Errorf("Get: unable to get fields from provided data: %v", err)
-	}
-
-	if len(fields.Fields) == 0 {
-		return fmt.Errorf("Get: input data is not provided")
-	}
-
-	id, ok := fields.Fields["ID"]
-	if ok {
-		return getByKey(client, fmt.Sprintf("%s", id), data)
-	}
-	return getByIndex(client, fields, data)
+	return get(client, req, data, true)
 }
 
-// First provides finding of the first element in the array
-func First(client *redis.Client, req, data interface{}) error {
-	fields, err := reflect.GetFullFields(req)
-	if err != nil {
-		return fmt.Errorf("Get: unable to get fields from provided data: %v", err)
-	}
-
-	if len(fields.Fields) == 0 {
-		return fmt.Errorf("Get: input data is not provided")
-	}
-
-	id, ok := fields.Fields["ID"]
-	if ok {
-		return getByKey(client, fmt.Sprintf("%s", id), data)
-	}
-	return nil
+// Last provides finding of the last element in the array
+func Last(client *redis.Client, req, data interface{}) error {
+	return get(client, req, data, false)
 }
 
 // GetByID provides getting data by id
 func GetByID(client *redis.Client, name string, ID interface{}, data interface{}) error {
-	return get(client, name, ID, data)
-}
-
-func get(client *redis.Client, name string, ID interface{}, data interface{}) error {
 	return getByKey(client, fmt.Sprintf("id:%v:%v", name, ID), data)
 }
 
+// general method for get value
+func get(client *redis.Client, req, data interface{}, asc bool) error {
+	fields, err := reflect.GetFullFields(req)
+	if err != nil {
+		return fmt.Errorf("Get: unable to get fields from provided data: %v", err)
+	}
+
+	if len(fields.Fields) == 0 {
+		return fmt.Errorf("Get: input data is not provided")
+	}
+
+	id, ok := fields.Fields["ID"]
+	if ok {
+		return getByKey(client, fmt.Sprintf("%s", id), data)
+	}
+	return getByIndex(client, fields, asc, data)
+}
+
 func getByKey(client *redis.Client, name string, data interface{}) error {
-	fmt.Println("PARENTID: ", name)
 	objStr, err := client.Do("GET", name).String()
 	if err != nil {
 		return errors.Wrap(err, "unable to find by the key")
@@ -72,7 +58,8 @@ func getByKey(client *redis.Client, name string, data interface{}) error {
 }
 
 // getByIndex provides getting of value by the index
-func getByIndex(client *redis.Client, fields *models.Search, data interface{}) error {
+// NOTE: At this moment, it takes only first attribute
+func getByIndex(client *redis.Client, fields *models.Search, asc bool, data interface{}) error {
 	for name, value := range fields.Fields {
 		valueStr := strings.ToLower(fmt.Sprintf("%v", value))
 		members, err := client.SMembers(valueStr).Result()
@@ -82,7 +69,11 @@ func getByIndex(client *redis.Client, fields *models.Search, data interface{}) e
 		if len(members) == 0 {
 			return fmt.Errorf("unable to find members by the name: %s", name)
 		}
-		return getByKey(client, members[0], data)
+		member := members[0]
+		if !asc {
+			member = members[len(members)-1]
+		}
+		return getByKey(client, member, data)
 	}
 	return nil
 }
