b7f58db6cacf04d4dfe6cefce37de5121bdbda37
Sergey <xxsmotur@gmail.com>
2020-03-14 01:02:17 +0500

    Adding of SetType for the set definition

--- config.go
+++ config.go
@@ -2,6 +2,14 @@ package reorm
 
 import "github.com/saromanov/re-orm/internal/serializer"
 
+// SetType defines type for teh set for index insering
+type SetType int
+
+var (
+	ZADD SetType = 0
+	SADD SetType = 1
+)
+
 // Config defines configuration for the project
 type Config struct {
 	Address    string
@@ -10,4 +18,5 @@ type Config struct {
 	// KeyPrefix returns prefix where data will be saved
 	// for example "prefix_id"
 	KeyPrefix string
+	SetType   SetType
 }
