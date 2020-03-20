bd350191a5b81d813b8f6558d14c40652956faed
Sergey <xxsmotur@gmail.com>
2020-03-21 00:27:11 +0500

    init for testing for save

--- internal/storage/save_test.go
+++ internal/storage/save_test.go
@@ -1,1 +1,34 @@
 package storage
+
+import (
+	"testing"
+
+	"github.com/go-redis/redis"
+	"github.com/stretchr/testify/assert"
+)
+
+var client = redis.NewClient(&redis.Options{
+	Addr: "localhost:6379",
+	DB:   0,
+})
+
+type Animal struct {
+	ID    int
+	Title string
+	Name  string
+	Color string
+	Type  int
+}
+
+func TestSaveBasic(t *testing.T) {
+	a := &Animal{
+		ID:    1,
+		Title: "Dog",
+		Name:  "Bob",
+		Color: "Black",
+		Type:  1,
+	}
+	resp, err := Save(client, a)
+	assert.NoError(t, err)
+	assert.Equal(t, resp, "")
+}
